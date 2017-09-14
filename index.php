495d76a333af453f36a57047bb22a7ffab056a5b
Francisco Fabregat <frank@fabregat.com.mx>
2017-09-13 21:38:17 -0500

    el audio funciona

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
 while(true || $asd==15)
 {
     sleep(1800); // sleep for 30 min
-	if (time("H") == 0) {
-		echo "<script>console.log( 'hi' );</script>";
-	}
+    if (time("H") == 0) {
+        echo "<script>console.log( 'hi' );</script>";
+    }
 
 }*/
 include 'config.php';
@@ -33,113 +33,143 @@ include 'config.php';
 $sqlcheckifnew = "SELECT * FROM properties";
 $resultcheckifnew = mysqli_query($db, $sqlcheckifnew);
 if (mysqli_num_rows($resultcheckifnew) == 0) {
-	$localIP = getHostByName(getHostName());
-	//echo $localIP;
-  $url = 'https://frank.fabregat.com.mx/cevideo/checkip.php?ip=' . $localIP;
-  //echo $url;
-	$json = file_get_contents('https://frank.fabregat.com.mx/cevideo/checkip.php?ip=' . $localIP);
-	$obj = json_decode($json);
-	$code = $obj->{'code'};
-	if ($code == "ERROR") {
-		echo "<br>ERROR: No existe hotel con IP '" . $localIP . "'";
-	} else {
-		//echo strtoupper($code);
-
-		include 'config.php';
-		$sqlupdate = "INSERT INTO `properties` (`code`, `version`) VALUES ('$code', '1')";
-		if (mysqli_query($db, $sqlupdate)) {
-      ?><meta http-equiv="refresh" content="0"><?php
+    $localIP = getHostByName(getHostName());
+    //echo $localIP;
+    $url = 'https://frank.fabregat.com.mx/cevideo/checkip.php?ip=' . $localIP;
+    //echo $url;
+    $json = file_get_contents('https://frank.fabregat.com.mx/cevideo/checkip.php?ip=' . $localIP);
+    $obj = json_decode($json);
+    $code = $obj->{'code'};
+    if ($code == "ERROR") {
+        echo "<br>ERROR: No existe hotel con IP '" . $localIP . "'";
     } else {
-      echo "No se ha podido modificar la base de datos";
+        //echo strtoupper($code);
+
+        include 'config.php';
+        $sqlupdate = "INSERT INTO `properties` (`code`, `version`) VALUES ('$code', '1')";
+        if (mysqli_query($db, $sqlupdate)) {
+            ?><meta http-equiv="refresh" content="0"><?php
+        } else {
+            echo "No se ha podido modificar la base de datos";
+        }
     }
-	}
 } else {
-	//echo "not new";
-	$row = mysqli_fetch_assoc($resultcheckifnew);
-	//echo strtoupper($row['code']);
+    //echo "not new";
+    $row = mysqli_fetch_assoc($resultcheckifnew);
+    //echo strtoupper($row['code']);
 
 
-	function updateVideo($code9, $version9, $output) {
-		$jsonu = file_get_contents('https://frank.fabregat.com.mx/cevideo/checkip.php?code='.$code9.'&version=' . $version9);
-	$obju = json_decode($jsonu);
-	$update = $obju->{'update'};
-		$newversion = $obju->{'version'};
-	if ($update == "yes") {
-		$source = "https://frank.fabregat.com.mx/cevideo/videos/".$row['code']."/video.mp4";
+    function updateVideo($code9, $version9, $output)
+    {
+        $jsonu = file_get_contents('https://frank.fabregat.com.mx/cevideo/checkip.php?code='.$code9.'&version=' . $version9);
+        $obju = json_decode($jsonu);
+        $update = $obju->{'update'};
+        $newversion = $obju->{'version'};
+        if ($update == "yes") {
+            /*$source = "https://frank.fabregat.com.mx/cevideo/videos/".$row['code']."/video.mp4";
 $dest = "video/video.mp4";
 if (copy($source, $dest)) {
-	if ($output == true) {
+    if ($output == true) {
 echo "<script>Alert.render('Se ha actualizado el video.','');</script>";
-	} else {
-			echo "<script>console.log('Se ha actualizado el video.');</script>";
-		}
-	include 'config.php';
-	$sqlversion = "UPDATE properties SET version='$newversion'";
-	$resultversion = mysqli_query($db, $sqlversion);
+    } else {
+                echo "<script>console.log('Se ha actualizado el video.');</script>";
+            }
+    include 'config.php';
+    $sqlversion = "UPDATE properties SET version='$newversion'";
+    $resultversion = mysqli_query($db, $sqlversion);
 } else {
-	if ($output == true) {
-	echo "<script>Alert.render('No se ha podido actualizar el video.','');</script>";
-	} else {
-			echo "<script>console.log('No se ha podido actualizar el video.');</script>";
-		}
-}
-	 } else {
-		if ($output == true) {
-		echo "<script>Alert.render('El video esta actualizado.','');</script>";
-		} else {
-			echo "<script>console.log('El video esta actualizado.');</script>";
-		}
-		//echo "dsa";
-	}
-	}
+    if ($output == true) {
+    echo "<script>Alert.render('No se ha podido actualizar el video.','');</script>";
+    } else {
+                echo "<script>console.log('No se ha podido actualizar el video.');</script>";
+            }
+}*/
+            $source = "https://frank.fabregat.com.mx/cevideo/videos/".$code9."/video.mp4";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $source);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            //curl_setopt($ch, CURLOPT_SSLVERSION,3);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $data = curl_exec($ch);
+            $error = curl_error($ch);
+            //var_dump(curl_getinfo($ch));
+            $resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
 
-	updateVideo($row['code'], $row['version'], false);
-	if (isset($_POST['update'])) {
-		updateVideo($row['code'], $row['version'], true);
-		/*$jsonu = file_get_contents('https://frank.fabregat.com.mx/cevideo/checkip.php?code='.$row['code'].'&version=' . $row['version']);
-	$obju = json_decode($jsonu);
-	$update = $obju->{'update'};
-		$newversion = $obju->{'version'};
-	if ($update == "yes") {
-		$source = "https://frank.fabregat.com.mx/cevideo/videos/".$row['code']."/video.mp4";
+            $destination = "video/video.mp4";
+            $file = fopen($destination, "wb");
+            if (fwrite($file, $data)) {
+                if ($output == true) {
+                    echo "<script>Alert.render('Se ha actualizado el video.','');</script>";
+                } else {
+                    echo "<script>console.log('Se ha actualizado el video.');</script>";
+                }
+                include 'config.php';
+                $sqlversion = "UPDATE properties SET version='$newversion'";
+                $resultversion = mysqli_query($db, $sqlversion);
+            } else {
+                if ($output == true) {
+                    echo "<script>Alert.render('No se ha podido actualizar el video.','');</script>";
+                } else {
+                    echo "<script>console.log('No se ha podido actualizar el video.');</script>";
+                }
+            }
+            fclose($file);
+        } else {
+            if ($output == true) {
+                echo "<script>Alert.render('El video esta actualizado.','');</script>";
+            } else {
+                echo "<script>console.log('El video esta actualizado.');</script>";
+            }
+            //echo "dsa";
+        }
+    }
+
+    updateVideo($row['code'], $row['version'], false);
+    if (isset($_POST['update'])) {
+        updateVideo($row['code'], $row['version'], true);
+        /*$jsonu = file_get_contents('https://frank.fabregat.com.mx/cevideo/checkip.php?code='.$row['code'].'&version=' . $row['version']);
+    $obju = json_decode($jsonu);
+    $update = $obju->{'update'};
+        $newversion = $obju->{'version'};
+    if ($update == "yes") {
+        $source = "https://frank.fabregat.com.mx/cevideo/videos/".$row['code']."/video.mp4";
 $dest = "video/video.mp4";
 if (copy($source, $dest)) {
 echo "<script>Alert.render('Se ha actualizado el video.','');</script>";
-	include 'config.php';
-	$sqlversion = "UPDATE properties SET version='$newversion'";
-	$resultversion = mysqli_query($db, $sqlversion);
+    include 'config.php';
+    $sqlversion = "UPDATE properties SET version='$newversion'";
+    $resultversion = mysqli_query($db, $sqlversion);
 } else {
-	echo "<script>Alert.render('No se ha podido actualizar el video.','');</script>";
+    echo "<script>Alert.render('No se ha podido actualizar el video.','');</script>";
 }
-	 } else {
-		echo "<script>Alert.render('El video esta actualizado.','');</script>";
-		//echo "dsa";
-	}*/
-	}
-  /*$localIP = getHostByName(getHostName());
-  $url = 'https://frank.fabregat.com.mx/cevideo/checkip.php?ip=' . $localIP;
-  echo $url;*/
-	?>
+     } else {
+        echo "<script>Alert.render('El video esta actualizado.','');</script>";
+        //echo "dsa";
+    }*/
+    }
+    /*$localIP = getHostByName(getHostName());
+    $url = 'https://frank.fabregat.com.mx/cevideo/checkip.php?ip=' . $localIP;
+    echo $url;*/ ?>
 <center><div class="heading"><a><?php echo strtoupper($row['code']); ?></a></div></center>
 <center><a class="btn" href="index.php?showvideo=true">Ver Video</a></center>
 <center><form action="" method="post"><input type="submit" value="Actualizar Video" name="update"></form></center>
 <?php
 if ($_GET['showvideo'] == "true") {
-?>
+        ?>
 <center>
 <div id="dialogoverlay2"></div>
 <div id="dialogbox2">
 	<div class="closebtn"><a href="index.php"><span class="icon-remove"></span></a></div>
 	<div>
-		<div id='dialogboxbody2'><video loop controls><source src="video/video.mp4"></video></div>
+		<div id='dialogboxbody2'><video loop controls autoplay><source src="video/video.mp4"></video></div>
 		</div>
 		</div>
 </center>
 
 <?php
-}
-	date_default_timezone_set('America/Mexico_City');
-	?>
+    }
+    date_default_timezone_set('America/Mexico_City'); ?>
 <div class="timer"><center><div id="timer_div"></div><a>Actualizado el <?php echo date('j'); ?>/<?php echo date('n'); ?>/<?php echo date('Y'); ?> a las <?php echo date('g'); ?>:<?php echo date('i'); ?>:<?php echo date('s'); ?><?php echo date('a'); ?></a></center></div>
 <script>
 	//var seconds_left = 30;
